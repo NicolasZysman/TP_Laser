1424ff994dccf795549ae7d19376fbe1b63d96e3
NicolasZysman <nzysman@gmail.com>
2024-09-26 21:02:25 -0300

    cambios

--- src/main/java/Juego.java
+++ src/main/java/Juego.java
@@ -2,16 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Juego {
-    public static void Main(String[] args) {
-        List<Nivel> niveles = new ArrayList<>();
+    private ArrayList<Nivel> niveles;
+    public Juego() {
+        ArrayList<Nivel> niveles = new ArrayList<>();
         int nivelActual;
+        System.out.println("Pepe");
     }
-
-    private void iniciarNivel(int nivelActual){
-        //recibe el nivel actual de vista y lo setea
-        //cambia el estado de juego si el nivel era 0?
-    }
-
-
+//    private void iniciarNivel(int nivelrecibidojavaFX){
+//        nivelActual = nivelrecibidojavaFX;
+//    }
 
 }
