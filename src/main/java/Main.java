74b8eb34fee3adaa9e4857e516fc97258594c090
“Oli03-git” <oweber@fi.uba.ar>
2024-09-26 21:03:45 -0300

    feat: Creación Grilla

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        Juego juego = new Juego();
+        juego.crearNivel(new File("../resources/levels/level1.dat"));
+    }
+}
--- src/main/java/Nivel.java
+++ src/main/java/Nivel.java
@@ -1,2 +1,32 @@
+import java.io.*;
+import java.util.Objects;
+
 public class Nivel {
+    private File archivo;
+    public Grilla grilla;
+
+    public Nivel(File informacion) {
+        this.archivo = informacion;
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        int fila = 0;
+        int columna = 0;
+        String fileName = archivo.getName();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(classLoader.getResourceAsStream("levels/" + fileName))))) {
+            String linea;
+            while ((linea = reader.readLine()) != null && !linea.isEmpty()) {
+                if (linea.length() > fila) {
+                    fila = linea.length();
+                }
+                columna++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        fila *= 2;
+        columna *= 2;
+
+        this.grilla = new Grilla(fila, columna);
+    }
 }
